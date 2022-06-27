99a73c84b2151edac19cd666194216768062a2ce
Mike-Apps <mikeott2022@outlook.com>
2022-06-27 16:23:08 -0400

    Add project files.

---
+++ Map_Test/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Map_Test;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Map_Test/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Map_Test;
+
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+    public MainPage()
+	{
+		InitializeComponent();
+  
+    }
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+
+        var mapControl = new Mapsui.UI.Maui.MapControl();
+        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        Content = mapControl;
+
+        //await Map.Default.OpenAsync(47.645160, -122.1306032);
+       
+	}
+}
+
